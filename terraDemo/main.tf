c8ec35f2c348fa2f5dad7207fd466fe8c9736922
Gabriel Fuentes <g_fuentes11@icloud.com>
2025-01-25 20:50:07 -0500

    updated with the terraDemo

---
+++ terraDemo/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "zoomcamp1-terraform"
+  region  = "us-east4-a"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "zoomcamp1-terraform-bucket"
+  location      = "US"
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
